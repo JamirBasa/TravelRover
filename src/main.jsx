579e418104ac5a7f21aced2cf91013659fd50afc
Dave Jamir Basa <basajamir@gmail.com>
2025-08-31 18:57:03 +0800

    Setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Header from './components/custom/Header.jsx'
 import CreateTrip from './create-trip/index.jsx'
+import { Toaster } from "sonner"
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Toaster />
     <RouterProvider router={router} />
   </StrictMode>,
 )
