1c13f0e79694fc9e240fa74eddeda9d2c9780608
Dave Jamir Basa <basajamir@gmail.com>
2025-09-18 11:35:32 +0800

    Actual Image Added

--- src/config/GlobalApi.jsx
+++ src/config/GlobalApi.jsx
@@ -7,13 +7,11 @@ const config={
     headers:{
         'Content-Type':'application/json',
         'X-Goog-Api-Key': import.meta.env.VITE_GOOGLE_PLACE_API_KEY,
-        'X-Goog-FieldMask': [
-            'places.id', 
-            'places.displayName', 
-            'places.photos'
-        ]
+        'X-Goog-FieldMask': 'places.id,places.displayName,places.photos'
     }
-}
+};
 
 
 export const GetPlaceDetails=(data)=>axios.post(BASE_URL, data, config)
+
+export const PHOTO_REF_URL='https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=600&maxWidthPx=600&key='+import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import React from 'react'
+import HotelCardItem from './HotelCardItem';
 
 function Hotels({ trip }) {
   return (
@@ -7,19 +8,7 @@ function Hotels({ trip }) {
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-3">
         {trip?.tripData?.tripData?.accommodations?.map((hotel, index) => (
-          <Link to={'https://www.google.com/maps/search/?api=1&query=' + hotel?.name} target="_blank">
-          <div key={index} className="border rounded-lg shadow p-3 hover:scale-105 transition-all cursor-pointer">
-            <img
-              src= "/placeholder.png"
-              alt={hotel?.name}
-              className="w-full h-40 object-cover rounded"
-            />
-            <h3 className="font-semibold mt-2">{hotel?.name}</h3>
-            <p className="text-sm text-gray-600">{hotel?.address}</p>
-            <p className="text-sm text-gray-800">{hotel?.pricePerNight || hotel?.priceRange}</p>
-            <p className="text-yellow-500 text-sm">⭐ {hotel?.rating}</p>
-          </div>
-          </Link>
+          <HotelCardItem hotel={hotel}/>
         ))}
       </div>
     </div>
