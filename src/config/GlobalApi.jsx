2db906bb937023d1e242464f29bce4de2eb4435e
pauldaniel25 <pauldanielojales@gmail.com>
2025-09-30 21:00:32 +0800

    push delete and fixes my trips

--- src/config/GlobalApi.jsx
+++ src/config/GlobalApi.jsx
@@ -95,7 +95,7 @@ export const GetPlaceDetails = async (data) => {
 
 export const PHOTO_REF_URL =
   "https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=600&maxWidthPx=600&key=" +
-  import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
+  import.meta.env.VITE_GOOGLE_PLACES_API_KEY;
 
 // ✅ Helper function to validate photo URLs
 export const validatePhotoUrl = (photoRef) => {
